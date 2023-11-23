9a9cddef1e3b425cf97edc4cb41b5c3d3f9ee519
Szymon Borkowski <szymonborkowski10@gmail.com>
2023-11-23 22:06:43 +0000

    update cartoon

--- cartoonify.py
+++ cartoonify.py
@@ -2,7 +2,7 @@ import cv2
 import sys
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 cap = cv2.VideoCapture(0)
